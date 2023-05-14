150458c4ec29f77c888dec1f46256b17e3e3907c
Daniel Bartholomae <daniel@bartholomae.name>
2023-05-14 22:18:03 +0200

    Add test for instance count in message
    Signed-off-by: Daniel Bartholomae <daniel@bartholomae.name>

--- src/action-helper.test.ts
+++ src/action-helper.test.ts
@@ -1,7 +1,26 @@
 import { createMessage } from "./action-helper";
+import { SiteFixture } from "./models/SiteFixture";
+import { AlertFixture } from "./models/AlertFixture";
+import { InstanceFixture } from "./models/InstanceFixture";
 
 describe("action-helper", () => {
   describe("createMessage", () => {
+    it("returns the count of instances for an alert", () => {
+      expect(
+        createMessage(
+          [
+            new SiteFixture({
+              alerts: [
+                new AlertFixture({ instances: [new InstanceFixture()] }),
+              ],
+            }),
+          ],
+          "2343454356",
+          "https://github.com/zaproxy/actions-common/actions/runs/4926339347"
+        )
+      ).toContain("total: 1");
+    });
+
     it("returns an empty string if there are no sites", () => {
       expect(createMessage([], "runnerId", "runnerLink")).toBe("");
     });
