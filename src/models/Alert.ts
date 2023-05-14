368e22f073fa8fb20dfa36202b2e8ab16710c79d
Daniel Bartholomae <daniel@bartholomae.name>
2023-05-14 22:30:36 +0200

    Add confidence and riskcode to Alert
    Signed-off-by: Daniel Bartholomae <daniel@bartholomae.name>

--- src/models/Alert.ts
+++ src/models/Alert.ts
@@ -3,5 +3,7 @@ import { Instance } from "./Instance";
 export interface Alert {
   name: string;
   pluginid: string;
+  riskcode: "0" | "1" | "2" | "3";
+  confidence: "0" | "1" | "2" | "3" | "4";
   instances: Instance[];
 }
