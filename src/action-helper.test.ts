2aef6adbace5c31e60850ca51238d100f9db59ba
Daniel Bartholomae <daniel@bartholomae.name>
2023-05-14 22:41:15 +0200

    Add a snapshot for the full message
    Signed-off-by: Daniel Bartholomae <daniel@bartholomae.name>

--- src/action-helper.test.ts
+++ src/action-helper.test.ts
@@ -5,6 +5,42 @@ import { InstanceFixture } from "./models/InstanceFixture";
 
 describe("action-helper", () => {
   describe("createMessage", () => {
+    it("returns the snapshot message", () => {
+      expect(
+        createMessage(
+          [
+            {
+              "@name": "http://localhost:8080",
+              alerts: [
+                {
+                  name: "Cross Site Scripting (Reflected)",
+                  pluginid: "40012",
+                  riskcode: "2",
+                  confidence: "3",
+                  instances: [
+                    { uri: "http://localhost:8080/bodgeit/contact.jsp" },
+                  ],
+                },
+              ],
+            },
+          ],
+
+          "2343454356",
+          "https://github.com/zaproxy/actions-common/actions/runs/4926339347"
+        )
+      ).toMatchInlineSnapshot(`
+        "- Site: [http://localhost:8080](http://localhost:8080) 
+         	 **New Alerts** 
+        	- **Medium risk (Confidence: High): Cross Site Scripting (Reflected)** [[40012]](https://www.zaproxy.org/docs/alerts/40012) total: 1:  
+        		- [http://localhost:8080/bodgeit/contact.jsp](http://localhost:8080/bodgeit/contact.jsp) 
+
+
+
+        https://github.com/zaproxy/actions-common/actions/runs/4926339347
+        2343454356"
+      `);
+    });
+
     it("returns the count of instances for an alert", () => {
       expect(
         createMessage(
